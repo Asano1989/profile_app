9a7c8e9693abc0edf14abfed3896b69bea85264c
Asano1989 <asano1989.b@gmail.com>
2025-06-27 15:49:47 +0900

    Docker設定修正、update:プロフィール作成フォーム

--- config/application.rb
+++ config/application.rb
@@ -18,10 +18,11 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module SampleRails
+module ProfileApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.2
+    config.i18n.default_locale = :ja
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
