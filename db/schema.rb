0a101d135ef640c217deb93cb3bd59a48eb28c43
Asano1989 <asano1989.b@gmail.com>
2025-07-04 19:28:48 +0900

    add:Deviseの導入・Userモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_07_02_004517) do
+ActiveRecord::Schema[7.2].define(version: 2025_07_04_022524) do
   create_table "profiles", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.text "profile_image"
@@ -41,5 +41,18 @@ ActiveRecord::Schema[7.2].define(version: 2025_07_02_004517) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "public_uid"
+  end
+
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "name", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
