d9355da8b2efaaafeb13ed09d3a1cb0e0dc23cc4
Asano1989 <asano1989.b@gmail.com>
2025-07-07 12:56:18 +0900

    Fix: CMD for Render port binding: entrypoint.sh comment-out-remove

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ bundle exec rails db:migrate
 exec bundle exec puma -C config/puma.rb
 
 # Remove a potentially pre-existing server.pid for Rails.
-# rm -f /profile_app/tmp/pids/server.pid
+rm -f /profile_app/tmp/pids/server.pid
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 # exec "$@"
