daf4860f47693d68197ac9c0b90c3a16e4365413
Asano1989 <asano1989.b@gmail.com>
2025-07-08 21:11:04 +0900

    update: 各ページレスポンシブ対応

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def page_title(page_title = "")
-    base_title = "★·̩͙✧*･Webプロフィール帳･٭☾·̩͙⋆★"
+    base_title = "Webプロフィール帳"
 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
